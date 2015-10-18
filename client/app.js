7e361a9fcc5b1b9fa003dd2ebe5fe4f3f949645d
Ryan Bogle <ryan@syrenmedia.com>
2015-10-17 18:36:26 -0700

    Added react and react-dom deps

--- client/app.js
+++ client/app.js
@@ -3,6 +3,20 @@
 import http from 'http';
 import alert from './fandive/api';
 
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-console.dir( http );
-alert( 'hello sports' );
+var grouper = function( name, dir ) {
+  console.group( `${name}:` );
+  console.dir( dir );
+  console.groupEnd();
+};
+
+grouper('http:', http);
+grouper('React:', React);
+grouper('ReactDOM:', ReactDOM);
+
+var div;
+document.body.appendChild( div = document.createElement( 'div' ) );
+div.innerHTML = 'hello sports';
+div.style.color = 'white';
